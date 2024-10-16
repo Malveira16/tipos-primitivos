8a97b237bad8803d1921f776d1fc3812f1ae3acf
João Vitor <joa.malveira@gmail.com>
2024-10-16 20:13:01 -0300

    Add files via upload

---
+++ array/README.md
@@ -0,0 +1,4 @@
+# Array
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Malveira16/pen/xxvdJdq](https://codepen.io/Malveira16/pen/xxvdJdq).
+
---
+++ array/src/script.js
@@ -0,0 +1,11 @@
+let livros = ["javascript", "ECMAScript", "MongoDB"]
+console.log(livros[1] [2])
+ resposta = prompt("Digite")
+
+let consulta = livros.indexOf("MongoDB")
+console.log(consulta)
+console.log(livros.pop())
+console.log(livros)
+console.log(livros.push("Python"))
+console.log(livros)
+
